fc962850de3e05302b8e80d4acd48a81bcae534d
ftozelli <felipe.tozelli@gmail.com>
2020-06-08 15:59:01 -0300

    vai que vai

---
+++ ExemploContas.java
@@ -0,0 +1,9 @@
+public class ExemploContas{
+    public static void main(String args[]){
+
+        double x = 1.7;
+        float a = (float) x / 2.3f;
+
+        System.out.println("Valor de a eh de " + a);
+    }
+}
